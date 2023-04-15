1a5ef4851a3f4cf2daa84fabe99b75a70c87838e
kasa <kasatani1226@gmail.com>
2023-04-15 13:16:44 +0900

    refactor test file name

--- pacvim_test.go
+++ pacvim_test.go
@@ -2,7 +2,34 @@ package main
 
 import (
 	"testing"
+
+	termbox "github.com/nsf/termbox-go"
 )
+
+func Test_switchScene(t *testing.T) {
+	if testing.Short() {
+		t.Skip("skipping test in short mode.")
+	}
+	if err := termbox.Init(); err != nil {
+		t.Error(err)
+	}
+	if err := termbox.Clear(termbox.ColorWhite, termbox.ColorBlack); err != nil {
+		t.Error(err)
+	}
+	defer termbox.Close()
+	scenes := []string{
+		sceneStart,
+		sceneYouwin,
+		sceneYoulose,
+		sceneCongrats,
+		sceneGoodbye,
+	}
+	for _, s := range scenes {
+		if err := switchScene(s); err != nil {
+			t.Error(err)
+		}
+	}
+}
 
 func Test_isInputNum(t *testing.T) {
 	type expectedValues struct {
