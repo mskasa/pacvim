17d21ed57c3f24df45deaf3951d29f0d876cb8bc
kasa <kasatani1226@gmail.com>
2023-04-17 09:33:43 +0900

    refactor

--- pacvim_test.go
+++ pacvim_test.go
@@ -8,94 +8,6 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
-func Test_switchScene(t *testing.T) {
-	if testing.Short() {
-		t.Skip("skipping test in short mode.")
-	}
-	if err := termbox.Init(); err != nil {
-		t.Error(err)
-	}
-	if err := termbox.Clear(termbox.ColorWhite, termbox.ColorBlack); err != nil {
-		t.Error(err)
-	}
-	defer termbox.Close()
-	scenes := []string{
-		sceneStart,
-		sceneYouwin,
-		sceneYoulose,
-		sceneCongrats,
-		sceneGoodbye,
-	}
-	for _, s := range scenes {
-		if err := switchScene(s); err != nil {
-			t.Error(err)
-		}
-	}
-}
-
-func Test_isInputNum(t *testing.T) {
-	type expectedValues struct {
-		s string
-		b bool
-	}
-	cases := map[string]struct {
-		player   player
-		arg      rune
-		expected expectedValues
-	}{
-		"Argument cannot be converted to a number.": {
-			player: player{
-				inputNum: 0,
-			},
-			arg: 'k',
-			expected: expectedValues{
-				s: "k",
-				b: false,
-			},
-		},
-		"Argument can be converted to a number.": {
-			player: player{
-				inputNum: 0,
-			},
-			arg: '2',
-			expected: expectedValues{
-				s: "2",
-				b: true,
-			},
-		},
-		"0 is the number 0.": {
-			player: player{
-				inputNum: 1,
-			},
-			arg: '0',
-			expected: expectedValues{
-				s: "0",
-				b: true,
-			},
-		},
-		"0 is the special string 0": {
-			player: player{
-				inputNum: 0,
-			},
-			arg: '0',
-			expected: expectedValues{
-				s: "0",
-				b: false,
-			},
-		},
-	}
-	for name, tt := range cases {
-		tt := tt
-		t.Run(name, func(t *testing.T) {
-			t.Parallel()
-			s, b := tt.player.isInputNum(tt.arg)
-			if s != tt.expected.s || b != tt.expected.b {
-				t.Error("expected:", tt.expected.s, tt.expected.b, "result:", s, b)
-			}
-		})
-	}
-}
-
 func Test_validateMimeType(t *testing.T) {
 	cases := map[string]struct {
 		mapPath  string
@@ -204,6 +116,69 @@ func Test_validateStageMap(t *testing.T) {
 	}
 }
 
+func Test_isInputNum(t *testing.T) {
+	type expectedValues struct {
+		s string
+		b bool
+	}
+	cases := map[string]struct {
+		player   player
+		arg      rune
+		expected expectedValues
+	}{
+		"Argument cannot be converted to a number.": {
+			player: player{
+				inputNum: 0,
+			},
+			arg: 'k',
+			expected: expectedValues{
+				s: "k",
+				b: false,
+			},
+		},
+		"Argument can be converted to a number.": {
+			player: player{
+				inputNum: 0,
+			},
+			arg: '2',
+			expected: expectedValues{
+				s: "2",
+				b: true,
+			},
+		},
+		"0 is the number 0.": {
+			player: player{
+				inputNum: 1,
+			},
+			arg: '0',
+			expected: expectedValues{
+				s: "0",
+				b: true,
+			},
+		},
+		"0 is the special string 0": {
+			player: player{
+				inputNum: 0,
+			},
+			arg: '0',
+			expected: expectedValues{
+				s: "0",
+				b: false,
+			},
+		},
+	}
+	for name, tt := range cases {
+		tt := tt
+		t.Run(name, func(t *testing.T) {
+			t.Parallel()
+			s, b := tt.player.isInputNum(tt.arg)
+			if s != tt.expected.s || b != tt.expected.b {
+				t.Error("expected:", tt.expected.s, tt.expected.b, "result:", s, b)
+			}
+		})
+	}
+}
+
 func Test_getDigit(t *testing.T) {
 	cases := map[string]struct {
 		linenum  int
@@ -256,5 +231,30 @@ func Test_makeLineNum(t *testing.T) {
 				t.Error("expected:", tt.expected, "result:", result)
 			}
 		})
+	}
+}
+
+func Test_switchScene(t *testing.T) {
+	if testing.Short() {
+		t.Skip("skipping test in short mode.")
+	}
+	if err := termbox.Init(); err != nil {
+		t.Error(err)
+	}
+	if err := termbox.Clear(termbox.ColorWhite, termbox.ColorBlack); err != nil {
+		t.Error(err)
+	}
+	defer termbox.Close()
+	scenes := []string{
+		sceneStart,
+		sceneYouwin,
+		sceneYoulose,
+		sceneCongrats,
+		sceneGoodbye,
+	}
+	for _, s := range scenes {
+		if err := switchScene(s); err != nil {
+			t.Error(err)
+		}
 	}
 }
