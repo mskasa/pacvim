7f0cad56bfeb842de39214e3ec0fcec05e8ee5da
kasa <kasatani1226@gmail.com>
2023-04-24 00:02:18 +0900

    update test

--- player_test.go
+++ player_test.go
@@ -80,3 +80,66 @@ func TestJumpOnCurrentLine(t *testing.T) {
 		})
 	}
 }
+
+func TestIsInputNum(t *testing.T) {
+	type expectedValues struct {
+		s string
+		b bool
+	}
+	cases := map[string]struct {
+		player   player
+		arg      rune
+		expected expectedValues
+	}{
+		"Argument cannot be converted to a number.": {
+			player: player{
+				inputNum: 0,
+			},
+			arg: 'k',
+			expected: expectedValues{
+				s: "k",
+				b: false,
+			},
+		},
+		"Argument can be converted to a number.": {
+			player: player{
+				inputNum: 0,
+			},
+			arg: '2',
+			expected: expectedValues{
+				s: "2",
+				b: true,
+			},
+		},
+		"0 is the number 0.": {
+			player: player{
+				inputNum: 1,
+			},
+			arg: '0',
+			expected: expectedValues{
+				s: "0",
+				b: true,
+			},
+		},
+		"0 is the special string 0": {
+			player: player{
+				inputNum: 0,
+			},
+			arg: '0',
+			expected: expectedValues{
+				s: "0",
+				b: false,
+			},
+		},
+	}
+	for name, tt := range cases {
+		tt := tt
+		t.Run(name, func(t *testing.T) {
+			t.Parallel()
+			s, b := tt.player.isInputNum(tt.arg)
+			if s != tt.expected.s || b != tt.expected.b {
+				t.Error("expected:", tt.expected.s, tt.expected.b, "result:", s, b)
+			}
+		})
+	}
+}
