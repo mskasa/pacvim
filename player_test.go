8cf34bca2dd351908767c4905748203e7ecdd509
kasa <kasatani1226@gmail.com>
2023-04-24 10:03:27 +0900

    add test code

--- player_test.go
+++ player_test.go
@@ -7,39 +7,135 @@ import (
 	termbox "github.com/nsf/termbox-go"
 )
 
-func TestJumpOnCurrentLine(t *testing.T) {
+func TestMoveCross(t *testing.T) {
+	initX := 9
+	initY := 4
 	cases := map[string]struct {
-		initX        int
+		x         int
+		y         int
+		expectedX int
+		expectedY int
+		inputNum  int
+	}{
+		"left": {
+			x:         -1,
+			y:         0,
+			expectedX: initX - 1,
+			expectedY: initY,
+			inputNum:  0,
+		},
+		"left with number": {
+			x:         -1,
+			y:         0,
+			expectedX: initX - 3,
+			expectedY: initY,
+			inputNum:  3,
+		},
+		"right": {
+			x:         1,
+			y:         0,
+			expectedX: initX + 1,
+			expectedY: initY,
+			inputNum:  0,
+		},
+		"right with number": {
+			x:         1,
+			y:         0,
+			expectedX: initX + 3,
+			expectedY: initY,
+			inputNum:  3,
+		},
+		"up": {
+			x:         0,
+			y:         -1,
+			expectedX: initX,
+			expectedY: initY - 1,
+			inputNum:  0,
+		},
+		"up with number": {
+			x:         0,
+			y:         -1,
+			expectedX: initX,
+			expectedY: initY - 2,
+			inputNum:  2,
+		},
+		"down": {
+			x:         0,
+			y:         1,
+			expectedX: initX,
+			expectedY: initY + 1,
+			inputNum:  0,
+		},
+		"down with number": {
+			x:         0,
+			y:         1,
+			expectedX: initX,
+			expectedY: initY + 2,
+			inputNum:  2,
+		},
+		"right with obstacle": {
+			x:         1,
+			y:         0,
+			expectedX: initX + 4,
+			expectedY: initY,
+			inputNum:  6,
+		},
+	}
+	if err := termbox.Init(); err != nil {
+		t.Error(err)
+	}
+	if err := termbox.Clear(termbox.ColorWhite, termbox.ColorBlack); err != nil {
+		t.Error(err)
+	}
+	defer termbox.Close()
+
+	p, err := playerActionTestInit("files/test/player/moveCross/map01.txt")
+	if err != nil {
+		t.Error()
+	}
+
+	for name, tt := range cases {
+		tt := tt
+		t.Run(name, func(t *testing.T) {
+			p.x = initX
+			p.y = initY
+			p.inputNum = tt.inputNum
+			p.moveCross(tt.x, tt.y)
+			if !(p.x == tt.expectedX && p.y == tt.expectedY) {
+				t.Error("expected:", tt.expectedX, tt.expectedY, "result:", p.x, p.y)
+			}
+		})
+	}
+}
+
+func TestJumpOnCurrentLine(t *testing.T) {
+	initX := 16
+	cases := map[string]struct {
 		initY        int
 		toLeftEdgeX  int
 		toRightEdgeX int
 	}{
 		"blank": {
-			initX:        16,
 			initY:        1,
 			toLeftEdgeX:  3,
 			toRightEdgeX: 29,
 		},
 		"target": {
-			initX:        16,
 			initY:        2,
 			toLeftEdgeX:  3,
 			toRightEdgeX: 29,
 		},
 		"obstacle": {
-			initX:        16,
 			initY:        3,
 			toLeftEdgeX:  6,
 			toRightEdgeX: 26,
 		},
 		"enemy": {
-			initX:        16,
 			initY:        4,
 			toLeftEdgeX:  3,
 			toRightEdgeX: 29,
 		},
 		"poison": {
-			initX:        16,
 			initY:        5,
 			toLeftEdgeX:  3,
 			toRightEdgeX: 29,
@@ -53,27 +149,16 @@ func TestJumpOnCurrentLine(t *testing.T) {
 		t.Error(err)
 	}
 	defer termbox.Close()
-	stage := stage{
-		mapPath:       "files/test/player/jumpOnCurrentLine/map01.txt",
-		hunterBuilder: newEnemyBuilder().defaultHunter(),
-		ghostBuilder:  newEnemyBuilder().defaultGhost(),
-	}
-	f, err := static.ReadFile(stage.mapPath)
+
+	p, err := playerActionTestInit("files/test/player/jumpOnCurrentLine/map01.txt")
 	if err != nil {
 		t.Error()
 	}
-	b := createBuffer(bytes.NewReader(f))
-	w := createWindow(b)
-	if err = w.show(b); err != nil {
-		t.Error()
-	}
-	p := new(player)
-	stage.plot(b, p)
 
 	for name, tt := range cases {
 		tt := tt
 		t.Run(name, func(t *testing.T) {
-			p.x = tt.initX
+			p.x = initX
 			p.y = tt.initY
 			p.jumpOnCurrentLine(p.toLeftEdge)
 			if !(p.x == tt.toLeftEdgeX && p.y == tt.initY) {
@@ -85,6 +170,26 @@ func TestJumpOnCurrentLine(t *testing.T) {
 			}
 		})
 	}
+}
+
+func playerActionTestInit(mapPath string) (*player, error) {
+	stage := stage{
+		mapPath:       mapPath,
+		hunterBuilder: newEnemyBuilder().defaultHunter(),
+		ghostBuilder:  newEnemyBuilder().defaultGhost(),
+	}
+	f, err := static.ReadFile(stage.mapPath)
+	if err != nil {
+		return nil, err
+	}
+	b := createBuffer(bytes.NewReader(f))
+	w := createWindow(b)
+	if err = w.show(b); err != nil {
+		return nil, err
+	}
+	p := new(player)
+	stage.plot(b, p)
+	return p, nil
 }
 
 func TestIsInputNum(t *testing.T) {
