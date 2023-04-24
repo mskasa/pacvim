de84ef99554cb9d03dcb07d55ca22d34a855a49e
kasa <kasatani1226@gmail.com>
2023-04-24 16:52:08 +0900

    add test code

--- pacvim_test.go
+++ pacvim_test.go
@@ -112,3 +112,39 @@ func TestValidateStageMap(t *testing.T) {
 		})
 	}
 }
+
+func TestValidateFiles(t *testing.T) {
+	cases := map[string]struct {
+		mapPath  string
+		expected string
+	}{
+		"normal": {
+			mapPath:  "files/test/validateMimeType/map01.txt",
+			expected: "",
+		},
+		"error/validateMimeType": {
+			mapPath:  "files/test/validateMimeType/map02.txt",
+			expected: "MIME Type Validation Error: files/test/validateMimeType/map02.txt; Invalid mime type: application/octet-stream;",
+		},
+		"error/validateFileSize": {
+			mapPath:  "files/test/validateFileSize/map02.txt",
+			expected: "File Size Validation Error: files/test/validateFileSize/map02.txt; File size exceeded: 1049 (Max file size is 1024);",
+		},
+		"error/validateStageMap": {
+			mapPath:  "files/test/validateStageMap/map05.txt",
+			expected: "Stage Map Validation Error: files/test/validateStageMap/map05.txt; Create a boundary for the stage map with '+' (line 1,15);",
+		},
+	}
+	for name, tt := range cases {
+		tt := tt
+		t.Run(name, func(t *testing.T) {
+			t.Parallel()
+			stages := []stage{
+				{mapPath: tt.mapPath},
+			}
+			if result := validateFiles(stages); result != nil {
+				assert.EqualErrorf(t, result, tt.expected, "Error should be: %v, got: %v", tt.expected, result)
+			}
+		})
+	}
+}
